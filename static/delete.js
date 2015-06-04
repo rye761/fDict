47d943c437ef601d3de6aa9dc282e57b164c65e3
Ryan <ryebread761@gmail.com>
2015-06-03 20:48:37 -0600

    Ugh, useless forgotten console logs :(

--- static/delete.js
+++ static/delete.js
@@ -1,6 +1,5 @@
 function delete_entry() {
 	$.post(Flask.url_for('delete_def'), { definition_id: defid }, function(data) {
 		window.location.replace(Flask.url_for('index'));
-		console.log('It didn\'t redirect');
 	});
 }
